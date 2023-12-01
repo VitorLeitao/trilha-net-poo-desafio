dc0085cd7a10fd98cf889c912d465f23cf0204aa
Vitor Leitao <vml2@cin.ufpe.br>
2023-12-01 12:57:38 -0300

    feat: Adicionando testes atraves da funcao TestarSmarphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        // Testando Nokia
+        Nokia nokia = new Nokia("123456", "Nokia Tijolao", "000000", 64);
+        TestarSmartphone(nokia, "Jogo da cobrinha");
+
+        // Testando Iphone
+        Iphone iphone = new Iphone("789012", "iPhone 13", "1111", 256);
+        TestarSmartphone(iphone, "TikTok");
+
+    }
+
+    // FUnção para testar TODAS as funções
+    static void TestarSmartphone(Smartphone smartphone, string nomeApp)
+    {
+        Console.WriteLine($"Informações do smartphone - Modelo: {smartphone.Modelo}, Memória: {smartphone.Memoria}GB");
+        smartphone.Ligar();
+        smartphone.ReceberLigacao();
+        smartphone.InstalarAplicativo(nomeApp);
+        Console.WriteLine();
+    }
+}
